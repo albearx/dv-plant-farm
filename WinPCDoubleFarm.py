d3d45866d86bdcaea58091ffed7161d4a7992e94
Albert Xu <albert.xu.13103@icloud.com>
2023-02-20 17:50:34 -0800

    Added Discord Bot functionality
    Added a Discord Bot to be notified on Discord when program terminates

--- WinPCDoubleFarm.py
+++ WinPCDoubleFarm.py
@@ -1,5 +1,8 @@
 import pyautogui
 import time
+import discord
+import config
+from discord.ext import commands
 
 # Overarching loop for macro.
 # Preconditions: Breeding cave and nursery are on the same screen when completely
@@ -18,6 +21,8 @@ import time
 # Step 9: Use image hashing to reselect the breeding cave
 
 iteration = 1
+time_str = '0s'
+total_runtime = 0
 
 def find_and_click(image_path, conf, step):
 
@@ -27,8 +32,7 @@ def find_and_click(image_path, conf, step):
   while (not success):
 
     if (fail_count >= 100):
-      print("Too many consecutive fails, terminating at iteration %d" % iteration)
-      exit()
+      raise Exception()
 
     loc = pyautogui.locateOnScreen(image_path, confidence = conf)
 
@@ -39,23 +43,23 @@ def find_and_click(image_path, conf, step):
 
       # Case where the Nursery HUD sometimes dips at the exact moment of click
       # and the default menu appears
-      if (step == 'Hatch Plant Egg'):
+      # if (step == 'Hatch Plant Egg'):
 
-        if (pyautogui.locateOnScreen('./WinPCImgs/rift.png', 0.95) is None):
-          success = True
-        else:
-          print('Avoided critical miss in iteration %d' % iteration)
-          find_and_click('./WinPCImgs/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail')
-          find_and_click('./WinPCImgs/hatch_plant_egg.png', 0.98, 'Hatch Plant Egg')
+      #   if (pyautogui.locateOnScreen('./WinPCImgs/rift.png', 0.95) is None):
+      #     success = True
+      #   else:
+      #     print('Avoided critical miss in iteration %d' % iteration)
+      #     find_and_click('./WinPCImgs/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail')
+      #     find_and_click('./WinPCImgs/hatch_plant_egg.png', 0.98, 'Hatch Plant Egg')
 
-      if (step == 'Hatch Plant Egg (Small)'):
+      # if (step == 'Hatch Plant Egg (Small)'):
 
-        if (pyautogui.locateOnScreen('./WinPCImgsSmall/rift.png', 0.95) is None):
-          success = True
-        else:
-          print('Avoided critical miss in iteration %d' % iteration)
-          find_and_click('./WinPCImgsSmall/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail')
-          find_and_click('./WinPCImgsSmall/hatch_plant_egg.png', 0.98, 'Hatch Plant Egg')
+      #   if (pyautogui.locateOnScreen('./WinPCImgsSmall/rift.png', 0.95) is None):
+      #     success = True
+      #   else:
+      #     print('Avoided critical miss in iteration %d' % iteration)
+      #     find_and_click('./WinPCImgsSmall/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail')
+      #     find_and_click('./WinPCImgsSmall/hatch_plant_egg.png', 0.98, 'Hatch Plant Egg')
 
       success = True
       print('Executing %s in iteration %d' % (step, iteration))
@@ -63,14 +67,14 @@ def find_and_click(image_path, conf, step):
     else:
       
       # Case where the Nursery HUD dips at the exact moment of click and the program clicks bottom left
-      if (step == 'Sell Button' and fail_count > 6):
-        find_and_click('./WinPCImgs/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail 2')
+      if (step == 'Sell Button' and fail_count > 3):
+        find_and_click('./WinPCImgs/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail')
         find_and_click('./WinPCImgs/hatch_plant_egg.png', 0.98, 'Hatch Plant Egg')
         find_and_click('./WinPCImgs/sell_button.png', 0.98, 'Sell Button')
         success = True
 
-      if (step == 'Sell Button (Small)' and fail_count > 6):
-        find_and_click('./WinPCImgsSmall/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail 2')
+      if (step == 'Sell Button (Small)' and fail_count > 3):
+        find_and_click('./WinPCImgsSmall/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail')
         find_and_click('./WinPCImgsSmall/hatch_plant_egg.png', 0.98, 'Hatch Plant Egg')
         find_and_click('./WinPCImgsSmall/sell_button.png', 0.98, 'Sell Button')
         success = True
@@ -79,11 +83,13 @@ def find_and_click(image_path, conf, step):
       print('%s not found in iteration %d, trying again.' % (step, iteration))
     #   time.sleep(0.2)
 
-#   time.sleep(0.2)
 
 
-def main():
+def plant_farm():
   global iteration
+  global time_str
+  global total_runtime
+
   start = time.time()
 
   while True: 
@@ -115,7 +121,7 @@ def main():
 
 		# Step 7: If heart is over breeding cave, click it
     find_and_click('./WinPCImgs/breed_heart.png', 0.9, 'Breed Complete')
-    find_and_click('./WinPCImgsSmall/breed_heart.png', 0.95, 'Yes Button')
+    find_and_click('./WinPCImgsSmall/breed_heart.png', 0.95, 'Breed Complete')
 
 		# Step 8: If "Place in Nursery" button is present, click it
     find_and_click('./WinPCImgs/place_in_nursery.png', 0.95, 'Place in Nursery')
@@ -133,4 +139,22 @@ def main():
     iteration += 1
 	#   time.sleep(1)
 
-main()
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='Hi Phil, ', intents=intents)
+
+@bot.event
+async def on_ready():
+  await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Plant Farming Simulator"))
+  channel = bot.get_channel(config.CHANNEL_ID)
+  await channel.send("`Beginning plant farm on Ferf's park and Bear's park`")
+  try:
+    plant_farm()
+  except:
+    print('Plant Farm terminated')
+
+  global iteration
+
+  await channel.send("`Plant farm process terminated after earning %d event currency per park (%d on double days) in %s, with an avg of %d EC/min (%d EC/min on double days).`" % (iteration * 3, iteration * 6, time_str, (iteration * 3) / (total_runtime / 60), (iteration * 6) / (total_runtime / 60)))
+  exit()
+
+bot.run(config.BOT_TOKEN)
