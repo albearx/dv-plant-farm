f8c01480002d887b46e829200e1e53e52825cc45
Albert Xu <albert.xu.13103@icloud.com>
2023-02-23 04:17:40 -0500

    Increased robustness
    Allows the script to relaunch the game if a connection error appears, and accounts for nursery full pop up.

---
+++ PlantFarm.py
@@ -0,0 +1,237 @@
+import config
+import discord
+import pyautogui
+import time
+
+from discord.ext import commands
+from termcolor import colored
+
+# Preconditions: Breeding cave and nursery are on the same screen when completely
+# zoomed in with either selected.
+# Nursery has a plant dragon egg in any nursery slot.
+# Breeding cave is currently selected and empty, with the prior breed being two plant dragons.
+# 
+# Step 1: If retry breed button is present, click it
+# Step 2: If breed button is present, click it
+# Step 3: Use image hashing to select the nursery
+# Step 4: If plant egg on left is present with NO TIMER, click it
+# Step 5: If sell button is present, click it
+# Step 6: If yes button is present, click it
+# Step 7: If heart is over breeding cave, click it
+# Step 8: If "Place in Nursery" button is present, click it
+# Step 9: Use image hashing to reselect the breeding cave
+
+img_directory = './WinLaptopImgs'
+iteration = 1
+time_str = '0s'
+total_runtime = 0
+start = time.time()
+run = True
+reset_count = 0
+
+# Assumes the view of park includes both the nursery and the cave
+# Sets preconditions for plant_farm()
+def set_preconditions():
+  attempts = 0
+  nursery_has_egg = False
+  breed_complete = False
+
+  while nursery_has_egg is False and attempts < 10:
+    if pyautogui.locateOnScreen(img_directory + '/nursery.png', confidence = 0.95) is not None:
+      print(colored('Nursery has plant egg', 'yellow'))
+      nursery_has_egg = True
+    attempts += 1
+
+  attempts = 0
+  while breed_complete is False and attempts < 10:
+    if pyautogui.locateOnScreen(img_directory + '/breed_heart.png', confidence = 0.95) is not None:
+      print(colored('Cave is occupied', 'yellow'))
+      breed_complete = True
+    attempts += 1
+  if (nursery_has_egg and breed_complete):
+    print(colored('Set Preconditions Case 1', 'yellow'))
+    find_and_click(img_directory + '/nursery.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/hatch_plant_egg.png', 0.9, 'Hatch Plant Egg')
+    find_and_click(img_directory + '/sell_button.png', 0.95, 'Sell Button')
+    find_and_click(img_directory + '/yes_button.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/breed_heart.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/place_in_nursery.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/breeding_cave.png', 0.95, 'Set Preconditions')
+  elif nursery_has_egg:
+    print(colored('Set Preconditions Case 2', 'yellow'))
+    find_and_click(img_directory + '/breeding_cave.png', 0.95, 'Set Preconditions')
+  elif breed_complete:
+    print(colored('Set Preconditions Case 3', 'yellow'))
+    find_and_click(img_directory + '/breed_heart.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/place_in_nursery.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/breeding_cave.png', 0.95, 'Set Preconditions')
+  else:
+    print(colored('Set Preconditions Case 4', 'yellow'))
+    find_and_click(img_directory + '/breeding_cave.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/breed_retry_button.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/breed_button.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/breed_heart.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/place_in_nursery.png', 0.95, 'Set Preconditions')
+    find_and_click(img_directory + '/breeding_cave.png', 0.95, 'Set Preconditions')
+
+# In the event of a game crash, relaunch the game and restart plant farm
+def reset_game():
+  global reset_count
+  reset_count += 1
+
+  loc = pyautogui.locateOnScreen(img_directory + '/continue.png', confidence=0.95)
+  if loc is None:
+    loc = pyautogui.locateOnScreen(img_directory + '/try_again.png', confidence=0.95)
+  
+  # If the game has crashed due to an in-game error, restart the game and bring view window back to farm area
+  if loc is not None:
+    pyautogui.click(pyautogui.center(loc))
+    
+    while (pyautogui.locateOnScreen(img_directory + '/goals_grey.png', confidence=0.95) is None and
+          pyautogui.locateOnScreen(img_directory + '/goals.png', confidence=0.95) is None):
+      time.sleep(10)
+
+    pyautogui.click(pyautogui.center(pyautogui.locateOnScreen(img_directory + '/macro_manager.png')))
+    time.sleep(1)
+    pyautogui.click(pyautogui.center(pyautogui.locateOnScreen(img_directory + '/play_macro.png')))
+    time.sleep(25) # should be at least the length of the macro used to move the view window
+  
+  # Else if there's somehow a nursery pop up that is preventing the program from continuing, close pop up
+  loc = pyautogui.locateOnScreen(img_directory + '/ok_button.png', confidence=0.95)
+  if loc is not None:
+    pyautogui.click(pyautogui.center(loc))
+  
+  # At this point, the nursery window should be in view.
+  # 10 attempts given to find the breeding cave; if breeding cave is not found then game condition is unfixable and terminate
+  attempts = 0
+  can_see_cave = False
+  while (can_see_cave is False and attempts < 10):
+    if (pyautogui.locateOnScreen(img_directory + '/breeding_cave.png', confidence=0.95) is not None):
+      can_see_cave = True
+    attempts += 1
+  
+  if can_see_cave is False:
+    raise Exception
+
+  # Set preconditions and restart farm
+  set_preconditions()
+  plant_farm()
+
+# Given an image path, find and click
+def find_and_click(image_path, conf, step):
+  success = False
+  fail_count = 0
+
+  while (not success):
+
+    if (fail_count >= 150):
+      print(colored('Too many consecutive fails, attempting to reset game', 'magenta'))
+      reset_game()
+
+    loc = pyautogui.locateOnScreen(image_path, confidence = conf)
+
+    if loc is not None:
+
+      target_center = pyautogui.center(loc)
+      pyautogui.click(target_center)
+
+      success = True
+      print(colored('Executing %s in iteration %d' % (step, iteration), 'green'))
+
+    else:
+      
+      # Case where the Nursery HUD dips at the exact moment of click and the program clicks bottom left
+      if (step == 'Hatch Plant Egg' and fail_count > 3):
+        print(colored('Avoided critical miss in iteration %d' % iteration, 'green'))
+        find_and_click(img_directory + '/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail 2')
+        find_and_click(img_directory + '/hatch_plant_egg.png', 0.9, 'Hatch Plant Egg')
+
+      if (step == 'Sell Button' and fail_count > 3):
+        print(colored('Avoided critical miss in iteration %d' % iteration, 'green'))
+        find_and_click(img_directory + '/nursery.png', 0.93, 'Avoid Nursery HUD Dip Fail 2')
+        find_and_click(img_directory + '/hatch_plant_egg.png', 0.9, 'Hatch Plant Egg')
+        find_and_click(img_directory + '/sell_button.png', 0.98, 'Sell Button')
+        success = True
+
+      fail_count += 1
+      print(colored('%s not found in iteration %d, trying again. Attempting game reset in %d more tries.' % (step, iteration, 150 - fail_count), 'red'))
+    time.sleep(0.2)
+
+
+def plant_farm():
+  global iteration
+  global time_str
+  global total_runtime
+  global start
+  global reset_count
+
+  while True: 
+		
+    iteration_start = time.time()
+    print(colored('Beginning iteration %d' % iteration, 'cyan'))
+		# Step 1: If retry breed button is present, click it
+    find_and_click(img_directory + '/breed_retry_button.png', 0.95, 'Retry Breed Button')
+    
+		# Step 2: If breed button is present, click it
+    find_and_click(img_directory + '/breed_button.png', 0.95, 'Breed Button')
+
+		# Step 3: Use image hashing to select the nursery
+    find_and_click(img_directory + '/nursery.png', 0.93, 'Nursery')
+
+		# Step 4: If plant egg on left is present with NO TIMER, click it
+    find_and_click(img_directory + '/hatch_plant_egg.png', 0.9, 'Hatch Plant Egg')
+
+		# Step 5: If sell button is present, click it
+    find_and_click(img_directory + '/sell_button.png', 0.95, 'Sell Button')
+    
+		# Step 6: If yes button is present, click it
+    find_and_click(img_directory + '/yes_button.png', 0.95, 'Yes Button')
+
+		# Step 7: If heart is over breeding cave, click it
+    find_and_click(img_directory + '/breed_heart.png', 0.9, 'Breed Complete')
+
+		# Step 8: If "Place in Nursery" button is present, click it
+    find_and_click(img_directory + '/place_in_nursery.png', 0.95, 'Place in Nursery')
+
+		# Step 9: Use image hashing to reselect the breeding cave
+    find_and_click(img_directory + '/breeding_cave.png', 0.95, 'Reselect Breeding Cave')
+
+    iteration_end = time.time()
+    total_runtime = time.time() - start
+    time_str = time.strftime("%H:%M:%S", time.gmtime(total_runtime))
+
+    print(colored('Finished iteration %d in %.2fs' % (iteration, (iteration_end - iteration_start)), 'cyan'))
+    print(colored('Total runtime: %s.' % time_str, 'cyan'))
+    print(colored('Total EC: %d (%d on double days)' % (iteration * 3, iteration * 6), 'cyan'))
+    print(colored('Average rate: %.1f EC/min (%.1f EC/min on double days)' % (float(iteration * 3) / (total_runtime / 60), float(iteration * 6) / (total_runtime / 60)), 'cyan'))
+    print(colored('Reset count: %d' % reset_count, 'cyan'))
+    print(colored('========================================================\n', 'cyan'))
+    iteration += 1
+
+# intents = discord.Intents.all()
+# bot = commands.Bot(command_prefix='Hi Phil, ', intents=intents)
+
+# @bot.event
+# async def on_ready():
+#   await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Plant Farming Simulator"))
+#   channel = bot.get_channel(config.CHANNEL_ID)
+#   await channel.send("`Beginning plant farm on Ferf's park and Bear's park`")
+#   try:
+#     set_preconditions()
+#     plant_farm()
+#   except:
+#     print('Plant Farm terminated')
+
+#   global iteration
+
+#   await channel.send("`Plant farm process terminated after earning %d event currency per park (%d on double days) in %s, with an avg of %d EC/min (%d EC/min on double days).`" % (iteration * 3, iteration * 6, time_str, (iteration * 3) / (total_runtime / 60), (iteration * 6) / (total_runtime / 60)))
+#   exit()
+
+# bot.run(config.BOT_TOKEN)
+time.sleep(5)
+try:
+  set_preconditions()
+  print(colored('Preconditions set, beginning plant farm', 'yellow'))
+  plant_farm()
+except:
+  print('Plant Farm Terminated')
